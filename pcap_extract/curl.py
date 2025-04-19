f17ed84b1359b30109cd1e16dbd3ff969ec2e6f5
VyTrg <140966315+VyTrg@users.noreply.github.com>
2025-04-19 13:33:19 +0700

    update

--- pcap_extract/curl.py
+++ pcap_extract/curl.py
@@ -47,7 +47,4 @@ while time.time() < end_time:
     print(f"[+] Running: {command}")
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
 
-
-
-
     time.sleep(random.randint(10, 30))
