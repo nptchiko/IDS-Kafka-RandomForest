a57e7a00762d6db5fe94da21c07aa579980f249d
minhngoquang6@gmail.com <minhngoquang6@gmail.com>
2025-04-20 01:13:55 +0700

    update init database in mongodb + update yml file

--- backend/db/mongo-entrypoint/init-data.js
+++ backend/db/mongo-entrypoint/init-data.js
@@ -1,8 +1,8 @@
-db.createUser({
-    user: "$MONGO_INITDB_ROOT_USERNAME",
-    pwd: "$MONGO_INITDB_ROOT_PASSWORD",
-    roles: ["root"]
-});
+// db.createUser({
+//     user: "$MONGO_INITDB_ROOT_USERNAME",
+//     pwd: "$MONGO_INITDB_ROOT_PASSWORD",
+//     roles: ["root"]
+// });
 // Kết nối đến database 'test'
 db = db.getSiblingDB('test');
 
