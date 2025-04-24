194908b045daa854cb723d42aa8d4be05164caf8
miku <letthewindtellyou@gmail.com>
2025-04-24 14:40:34 +0700

    docs: syncing

--- backend/kafka/kafka-connect/deploy-kafka-connector.sh
+++ backend/kafka/kafka-connect/deploy-kafka-connector.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X POST -H "Content-Type: application/json" --data @kafka-connect.json http://localhost:8083/connectors
+curl -X POST -H "Content-Type: application/json" --data '@kafka-connect.json' http://localhost:8083/connectors
